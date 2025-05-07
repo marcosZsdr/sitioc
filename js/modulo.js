28da3e6a18e583d4a6666a75cacb0d13ed188a6a
marcosZsdr <tw.marcos.35@gmail.com>
2025-05-07 19:58:14 -0300

    se realizaron algunos cambios para que funcione todo el sitio

--- js/modulo.js
+++ js/modulo.js
@@ -8,22 +8,6 @@
       });
     }
 
-    /*Header*/
-    document.getElementById("header").innerHTML = `<header>
-            <div class="container">
-            <img src="img/plane-alt-solid-24.png" alt="logo" class="logo">
-            <h1>TITULO PRINCIPAL</h1>
-            <nav id="nav">
-                <button id="menu-toggle">☰</button>
-                <ul id="menu">
-                    <li><a href="#">INICIO</a></li>
-                    <li><a href="destinos.html">DESTINOS</a></li>
-                    <li><a href="contacto.html">CONTACTOS</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>`;
-
     /*Footer*/
     document.getElementById("footer").innerHTML = `<footer>
         <p style="color: white;">© Copyright 2025. Todos los derechos reservados.</p>
