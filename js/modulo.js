30f1af6b9e058b133e03e1002bd248ac9e59bf7f
marcos <tw.marcos.35@gmail.com>
2025-05-07 18:00:08 -0300

    el menu hamburguesa ya funciona y se hicieron algunas correcciones en el html y css

--- js/modulo.js
+++ js/modulo.js
@@ -1,15 +1,4 @@
-
-document.getElementById("header").innerHTML = "<h1>Explora Argentina</h1>";
-document.getElementById("nav").innerHTML = `  <ul>
-        <li><a href="index.html">Inicio</a></li>
-        <li><a href="pagina1.html">Destinos</a></li>
-        <li><a href="pagina2.html">Contacto</a></li>
-    </ul>
-`;
-document.getElementById("footer").innerHTML = '<p>&copy; 2025 Explora Argentina - Todos los derechos reservados</p>';
-
-/*menu hamburguesa */
-document.addEventListener("DOMContentLoaded", () => {
+    // Menú hamburguesa
     const toggle = document.getElementById("menu-toggle");
     const menu = document.getElementById("menu");
   
@@ -18,7 +7,30 @@ document.addEventListener("DOMContentLoaded", () => {
         menu.classList.toggle("show");
       });
     }
-  });
-  
-  
+
+    /*Header*/
+    document.getElementById("header").innerHTML = `<header>
+            <div class="container">
+            <img src="img/plane-alt-solid-24.png" alt="logo" class="logo">
+            <h1>TITULO PRINCIPAL</h1>
+            <nav id="nav">
+                <button id="menu-toggle">☰</button>
+                <ul id="menu">
+                    <li><a href="#">INICIO</a></li>
+                    <li><a href="destinos.html">DESTINOS</a></li>
+                    <li><a href="contacto.html">CONTACTOS</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>`;
+
+    /*Footer*/
+    document.getElementById("footer").innerHTML = `<footer>
+        <p style="color: white;">© Copyright 2025. Todos los derechos reservados.</p>
+        <div class="social-icons">
+            <a href="#"><img src="img/twitter.png"></a>
+            <a href="#"><img src="img/instagram-new.png"></a>
+            <a href="#"><img src="img/print.png"></a>
+        </div>
+    </footer>`;
   
